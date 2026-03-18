495b84c41bacebde37b1dbff5be5bde3a8c3166e
Muhammed-Hayr-Ali <m.thelord963@gmail.com>
2026-03-19 01:04:18 +0300

    rebuild

--- lib/actions/authentication/signIn-with-google.ts
+++ lib/actions/authentication/signIn-with-google.ts
@@ -8,7 +8,7 @@ export async function signInWithGoogle(locale: string): Promise<ApiResult> {
   const { error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${window.location.origin}/${locale}/callback`,
+      redirectTo: `${window.location.origin}/api/callback`,
       queryParams: {
         access_type: "offline",
         prompt: "consent",
